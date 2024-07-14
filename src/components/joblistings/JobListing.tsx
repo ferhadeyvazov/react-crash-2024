283b28db86fa057d9426564366b5dd3bf7ecfd86
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2024-07-15 00:28:36 +0400

    Job Listing is Ready

--- src/components/joblistings/JobListing.tsx
+++ src/components/joblistings/JobListing.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FaMapMarker} from 'react-icons/fa'
 
 type ICompany = {
     name: string;
@@ -50,7 +51,7 @@ const JobListing: React.FC<IProps> = ({ job }) => {
 
                 <div className='flex flex-col lg:flex-row justify-between mb-4'>
                     <div className='text-orange-700 mb-3'>
-                        {/* <FaMapMarker className='inline text-lg mb-1 mr-1' /> */}
+                        <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
                     <a
