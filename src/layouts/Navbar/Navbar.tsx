ac102b5523e854c2bf3a918c237912f529096d94
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2024-07-16 22:15:33 +0400

    a tag was changed Link which came from react-router-dom in Navbar component

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import {Link} from 'react-router-dom'
 import logo from '../../assets/logo.png'
 
 const Navbar = () => {
@@ -6,23 +7,23 @@ const Navbar = () => {
             <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
                 <div className='flex h-20 items-center justify-between'>
                     <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-                        <a className='flex flex-shrink-0 items-center mr-4' href='/'>
+                        <Link className='flex flex-shrink-0 items-center mr-4' to='/'>
                             <img className='h-10 w-auto' src={logo} alt='React Jobs' />
                             <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                                 React Jobs
                             </span>
-                        </a>
+                        </Link>
                         <div className='md:ml-auto'>
                             <div className='flex space-x-2'>
-                                <a href='/'>
+                                <Link to='/'>
                                     Home
-                                </a>
-                                <a href='/jobs'>
+                                </Link>
+                                <Link to='/jobs'>
                                     Jobs
-                                </a>
-                                <a href='/add-job'>
+                                </Link>
+                                <Link to='/add-job'>
                                     Add Job
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
