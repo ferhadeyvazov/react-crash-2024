0ae509c1935e02cf35ecd8a9c97df4c8b374d8af
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2024-07-30 17:46:56 +0400

    New ViewAllJobs.tsx component was added and ready

--- src/components/view_all_jobs/ViewAllJobs.tsx
+++ src/components/view_all_jobs/ViewAllJobs.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const ViewAllJobs:React.FC = () => {
+const ViewAllJobs: React.FC = () => {
   return (
-    <div>ViewAllJobs</div>
+    <section className='m-auto max-w-lg my-10 px-6'>
+      <Link
+        to='/jobs'
+        className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
+      >
+        View All Jobs
+      </Link>
+    </section>
   )
 }
 
